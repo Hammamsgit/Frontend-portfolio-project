f9171c6c062ae4255f37d2936f32bd632bb986bd
Hammam Alzilitni <hammam@Hammams-MBP.broadband>
2022-03-07 15:32:55 +0000

    improved date feature

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import moment from 'moment'
 
-function ArticleCard({title,author,body,votes,date,comments}) {
-    const revisedDate = new Date(date)
+function ArticleCard({title,author,votes,date,comments}) {
+    const revisedDate= moment(date).utc().format('DD/MM/YYYY')
     return (
         <div className="card">
             <h4 className="authorTag">{author}</h4>
           <h2>{title} </h2>
           <div className="tools">
-          <p className="date"> Posted {date}</p>
+          <p className="date"> Posted {revisedDate}</p>
           <p className="votes">votes {votes}</p>
             <p className="commentNum">comments {comments}</p>
           </div>
